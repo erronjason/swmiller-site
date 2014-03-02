cad3a9b99c247d048d561ad08b899065422d06d9
swmiller944 <swmiller944@gmail.com>
2014-03-02 03:16:18 -0600

    q in "quotes" to Q

--- include/menu.php
+++ include/menu.php
@@ -3,7 +3,7 @@
 		<li><a href="index.php">Home</a></li>
 		<li><a href="index.php?l=about">About</a></li>
 		<li><a href="index.php?l=work">Work</a></li>
-		<li><a href="index.php?l=locationsprices">Locations and quotes</a></li>
+		<li><a href="index.php?l=locationsprices">Locations and Quotes</a></li>
 		<li><a href="index.php?l=contact">Contact</a></li>
 	</ul>
 </div>
