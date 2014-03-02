adfa1e0d6ed14bb19b132950b03e7ee15d00dfd4
swmiller944 <swmiller944@gmail.com>
2014-03-02 05:28:32 -0600

    added batter

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 		<link rel="icon" href="assets/img/favicon.ico" type="image/x-icon" />
 	</head>
 	<body>
+		<span class="banner"><img src="assets/img/banner.png" height="256" width="650"></span>
 		<span class="coffeestain"><img src="assets/img/coffeestain2.png" height="280" width="280"></span>
 	    <?php include 'include/menu.php'; ?>
 		<div class="main">
